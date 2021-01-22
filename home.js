2437aaf703ddc456b971c69583602720f898616e
Eric Stazzone <55301772+ericstazzone@users.noreply.github.com>
2021-01-22 00:19:49 -0500

    responsive

--- home.js
+++ home.js
@@ -12,3 +12,12 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 5000);
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
