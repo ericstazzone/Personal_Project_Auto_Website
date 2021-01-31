45df68495595c2cf5956a78611146652e70c4b3f
Eric Stazzone <55301772+ericstazzone@users.noreply.github.com>
2021-01-31 12:05:43 -0500

    simplified website

---
+++ main.js
@@ -0,0 +1,25 @@
+/* Nav Bar */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
+  /* Home Screen Slide Show */
+  var slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000);
+  }
